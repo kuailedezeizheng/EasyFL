f9a36e0b7613a915658b9bb3d87885f59b82fe2b
Elon Li <elonisme@keemail.me>
2024-03-07 11:01:32 +0800

    update small flame

--- defenses/small_flame.py
+++ defenses/small_flame.py
@@ -1,9 +1,8 @@
-import copy
-from collections import Counter, OrderedDict
+from collections import OrderedDict
 
-import hdbscan
 import torch
 
+from defenses.fed_avg import federated_averaging
 from defenses.flame import flame
 
 
@@ -38,79 +37,10 @@ def create_fc_layers_models(model_list, type_of_model):
         return fc_model_list
 
 
-# def flame_module(fc_model_list, global_fc_model, device):
-#     fc_avg = copy.deepcopy(global_fc_model)
-#     cos = []
-#     cos_ = []
-#
-#     for fc_model_out in fc_model_list:
-#         x1 = vectorize_net(fc_model_out) - vectorize_net(global_fc_model)
-#         for fc_model_in in fc_model_list:
-#             x2 = vectorize_net(fc_model_in) - vectorize_net(global_fc_model)
-#             cos.append(torch.cosine_similarity(x1, x2, dim=0).detach().cpu())
-#         cos_.append(torch.cat([p.view(-1) for p in cos]).reshape(-1, 1))
-#         cos = []
-#
-#     cos_ = torch.cat([p.view(1, -1) for p in cos_])
-#
-#     cluster_er = hdbscan.HDBSCAN(min_cluster_size=2)
-#     cluster_labels = cluster_er.fit_predict(cos_)
-#     majority = Counter(cluster_labels)
-#     res = majority.most_common(len(fc_model_list))
-#
-#     out = []  # 正常模型列表
-#
-#     for i in range(len(cluster_labels)):  # 筛选出正常模型
-#         if cluster_labels[i] == res[0][0]:
-#             out.append(i)
-#
-#     e = []  # 每个模型和全局模型的欧氏距离
-#     for i in range(len(fc_model_list)):
-#         e.append(
-#             torch.sqrt(
-#                 torch.sum(
-#                     (vectorize_net(fc_avg) -
-#                      vectorize_net(fc_model_list[i])) ** 2)))
-#
-#     e = torch.cat([p.view(-1) for p in e])
-#     st = torch.median(e)
-#     whole_aggregator = []
-#     par = []
-#
-#     for i in range(len(out)):
-#         par.append(min(1, st / e[out[i]]))
-#
-#     wa = []
-#     for p_index, p in enumerate(fc_avg):
-#         wa.append(fc_avg[p])
-#
-#     for p_index, p in enumerate(fc_model_list[0]):
-#         # initial
-#         params_aggregator = torch.zeros(fc_model_list[0][p].size()).to(device)
-#
-#         for i in range(len(out)):
-#             net = fc_model_list[out[i]]
-#             params_aggregator = params_aggregator + \
-#                 wa[p_index] + (net[p] - wa[p_index]) * par[i]
-#
-#         sum = 0
-#         for i in range(len(par)):
-#             sum += 1
-#         params_aggregator = params_aggregator / sum
-#         whole_aggregator.append(params_aggregator)
-#     lamda = 1e-3
-#     sigma = st * lamda
-#
-#     for param_index, p in enumerate(fc_avg):
-#         fc_avg[p] = whole_aggregator[param_index] + \
-#             (sigma ** 2) * torch.randn(whole_aggregator[param_index].shape).to(device)
-#
-#     return fc_avg
-
-
 def small_flame(model_list, global_model, device):
     global_fc_model = create_fc_layers_models(global_model, "global_model")
     fc_model_list = create_fc_layers_models(model_list, "fc_model_list")
     fc_avg = flame(fc_model_list, global_fc_model, device)
-    new_global_model = replace_fc_layers(global_model, fc_avg)
+    global_avg_model = federated_averaging(global_weight=global_model, w_list=model_list)
+    new_global_model = replace_fc_layers(global_avg_model, fc_avg)
     return new_global_model
